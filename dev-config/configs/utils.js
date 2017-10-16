c07509ad55f65fddb9430f12b5e129aebf3ee020
hubiqing <hubiqing01@58ganji.com>
2017-10-16 12:29:19 +0800

    css

--- dev-config/configs/utils.js
+++ dev-config/configs/utils.js
@@ -8,16 +8,7 @@
 //  exports.postCSSConfig = [
 //      autoprefixer({
 //          browsers: [
-//              'ie >= 9',
-//              'ie_mob >= 10',
-//              'ff >= 30',
-//              'chrome >= 34',
-//              'safari >= 7',
-//              'opera >= 23',
-//              'ios >= 7',
-//              'android >= 4.4',
-//              'bb >= 10'
+//            '> 5%'
 //          ]
 //      }),
-//      require('postcss-flexibility')
 //  ]
